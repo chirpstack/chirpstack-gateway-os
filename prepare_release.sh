bcc6d007e51d60e5520afb596bcdf4aceac8f3e4
Orne Brocaar <info@brocaar.com>
2025-02-21 12:56:56 +0000

    sensecap_m2: Update config + prepare_release script.
    Without `CONFIG_VERSION_CODE_FILENAMES`, the image does not contain
    `sensecap-m2`.

--- prepare_release.sh
+++ prepare_release.sh
@@ -20,3 +20,4 @@ do_prepare $1 rak bcm27xx bcm2709 rpi-2 rak7391
 do_prepare $1 rak ramips mt76x8 rakwireless_rak636 rak7268v2
 do_prepare $1 rak ramips mt76x8 rakwireless_rak636 rak7289v2
 do_prepare $1 rak ramips mt76x8 rakwireless_rak636 rak7267
+do_prepare $1 seeed ramips mt76x8 sensecap_wm7628n sensecap-m2
