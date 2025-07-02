9e8c5748b51a719387670ec6bae24e6219206b04
Orne Brocaar <info@brocaar.com>
2025-07-02 14:54:24 +0100

    Update prepare release script.

--- prepare_release.sh
+++ prepare_release.sh
@@ -16,6 +16,8 @@ do_prepare $1 raspberrypi bcm27xx bcm2708 rpi base
 do_prepare $1 raspberrypi bcm27xx bcm2708 rpi full
 do_prepare $1 raspberrypi bcm27xx bcm2709 rpi-2 base
 do_prepare $1 raspberrypi bcm27xx bcm2709 rpi-2 full
+do_prepare $1 raspberrypi bcm27xx bcm2712 rpi-5 base
+do_prepare $1 raspberrypi bcm27xx bcm2712 rpi-5 full
 do_prepare $1 rak bcm27xx bcm2709 rpi-2 rak7391
 do_prepare $1 rak ramips mt76x8 rakwireless_rak636 rak7268v2
 do_prepare $1 rak ramips mt76x8 rakwireless_rak636 rak7289v2
